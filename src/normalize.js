3f58ab06ff4213d30e0600bea4a635c7c2a8914e
takion <jo@surikat.pro>
2018-05-05 01:11:14 +0200

    add validator sanitizers to normalize

--- src/normalize.js
+++ src/normalize.js
@@ -1,7 +1,37 @@
+//validator
+import blacklist from 'validator/lib/blacklist'
+import escape from 'validator/lib/escape'
+import unescape from 'validator/lib/unescape'
+import ltrim from 'validator/lib/ltrim'
+import normalizeEmail from 'validator/lib/normalizeEmail'
+import rtrim from 'validator/lib/rtrim'
+import stripLow from 'validator/lib/stripLow'
+import toBoolean from 'validator/lib/toBoolean'
+import toDate from 'validator/lib/toDate'
+import toFloat from 'validator/lib/toFloat'
+import toInt from 'validator/lib/toInt'
+import trim from 'validator/lib/trim'
+import whitelist from 'validator/lib/whitelist'
+
 import siret from './normalizers/siret'
 import siren from './normalizers/siren'
+
 
 export default {
   siret,
   siren,
+  
+  blacklist,
+  escape,
+  unescape,
+  ltrim,
+  normalizeEmail,
+  rtrim,
+  stripLow,
+  toBoolean,
+  toDate,
+  toFloat,
+  toInt,
+  whitelist,
+  trim,
 }
