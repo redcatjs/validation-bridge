84371d6304b0d418468b42893310d6892fdbf970
takion <jo@surikat.pro>
2018-05-06 00:49:30 +0200

    string length improve

--- src/validators/string-length.js
+++ src/validators/string-length.js
@@ -2,15 +2,15 @@ import { isString, isNumber } from 'typeable'
 
 export default function stringLength (value, recipe = {}) {
   if (!isString(value)) return false
+  
+  if(typeof recipe !== 'object'){
+    recipe = { equal: recipe }
+  }
+  const { equal, min, max } = recipe
+  const len = value.length
 
-  const { bytes = false, min, minOrEqual, max, maxOrEqual } = recipe
-  const len = bytes
-    ? encodeURI(value).split(/%..|./).length - 1
-    : value.length
-
-  if (isNumber(min) && !(len > min)) return false
-  if (isNumber(minOrEqual) && !(len >= minOrEqual)) return false
-  if (isNumber(max) && !(len < max)) return false
-  if (isNumber(maxOrEqual) && !(len <= maxOrEqual)) return false
+  if (isNumber(equal) && !(len === equal)) return false
+  if (isNumber(min) && !(len >= min)) return false
+  if (isNumber(max) && !(len <= max)) return false
   return true
 }
