92d0b1717dd59de0d8218da2e9b9ca2e028de433
takion <jo@surikat.pro>
2018-05-06 01:53:18 +0200

    check string

--- src/validators/mysql.js
+++ src/validators/mysql.js
@@ -1,5 +1,7 @@
 import validator from 'mysql-validator'
+import { isString } from 'typeable'
 export default function mysql (value, columnType) {
+  if(!isString(value)) return false
   return !validator.check(value, columnType)
 }
 mysql.message = 'unexpected mysql value for this column'
