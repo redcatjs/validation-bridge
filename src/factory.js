bd3b3dc367f4168dd11f17e144977d5d77861cad
takion <jo@surikat.pro>
2018-05-06 13:39:23 +0200

    wrapper with default to unRequired

--- src/factory.js
+++ src/factory.js
@@ -1,9 +1,18 @@
-export default function (rules) {
+import { isPresent } from 'typeable'
+
+function defaultWrapper(callback, values, allValues){
+  if(!isPresent(value)){
+    return true
+  }
+  return callback(values, allValues)
+}
+
+export default function (rules, wrapper = defaultWrapper) {
   const ruleFactories = {}
   Object.entries(rules).forEach(([name, func]) => {
     ruleFactories[name] = function (options) {
-      return function (value, allValues) {
-        return func(value, options, allValues)
+      return function(values, allValues){
+        return wrapper(func, values, allValues)
       }
     }
   })
