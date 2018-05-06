7eb914660cead76a15ef40683d8d6f5b676903d9
takion <jo@surikat.pro>
2018-05-06 13:41:01 +0200

    fix + clean

--- src/factory.js
+++ src/factory.js
@@ -1,18 +1,18 @@
 import { isPresent } from 'typeable'
 
-function defaultWrapper(callback, values, allValues){
-  if(!isPresent(value)){
+function defaultWrapper (callback, value, allValues) {
+  if (!isPresent(value)) {
     return true
   }
-  return callback(values, allValues)
+  return callback(value, allValues)
 }
 
 export default function (rules, wrapper = defaultWrapper) {
   const ruleFactories = {}
   Object.entries(rules).forEach(([name, func]) => {
     ruleFactories[name] = function (options) {
-      return function(values, allValues){
-        return wrapper(func, values, allValues)
+      return function (value, allValues) {
+        return wrapper(func, value, allValues)
       }
     }
   })
