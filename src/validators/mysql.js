b007a864860f3bc2fef273ce3b6d450552934e93
takion <jo@surikat.pro>
2018-05-16 02:41:30 +0200

    mysql isScalar

--- src/validators/mysql.js
+++ src/validators/mysql.js
@@ -1,7 +1,7 @@
 import validator from 'mysql-validator'
-import { isString } from 'typeable'
+import isScalar from './is-scalar'
 export default function mysql (value, columnType) {
-  if (!isString(value)) return false
+  if (!isScalar(value)) return false
   return !validator.check(value, columnType)
 }
 mysql.message = 'unexpected mysql value for this column'
