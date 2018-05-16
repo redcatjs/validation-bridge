f088337118b6fe90a2a2fdc3e5cd6dc0d88caa94
takion <jo@surikat.pro>
2018-05-16 02:41:50 +0200

    cs

--- src/validators/is-scalar.js
+++ src/validators/is-scalar.js
@@ -1,17 +1,17 @@
-export default function isScalar(value, {
-  withSymbol = true,
-}){
-  const type = typeof(value)
-  if(type === 'string') return true
-  if(type === 'number') return true
-  if(type === 'boolean') return true
-  if(withSymbol === true && type === 'symbol') return true
+export default function isScalar (value, {
+  withSymbol = true
+}) {
+  const type = typeof (value)
+  if (type === 'string') return true
+  if (type === 'number') return true
+  if (type === 'boolean') return true
+  if (withSymbol === true && type === 'symbol') return true
 
-  if(value == null) return true;
-  if(withSymbol === true && value instanceof Symbol) return true
-  if(value instanceof String) return true
-  if(value instanceof Number) return true
-  if(value instanceof Boolean) return true
+  if (value == null) return true
+  if (withSymbol === true && value instanceof Symbol) return true
+  if (value instanceof String) return true
+  if (value instanceof Number) return true
+  if (value instanceof Boolean) return true
 
   return false
 }
