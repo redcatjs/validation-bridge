3d9f5d480dce1844f346d794933d877f03eaa117
takion <jo@surikat.pro>
2018-05-20 01:18:24 +0200

    fix

--- src/validators/array-in.js
+++ src/validators/array-in.js
@@ -1,6 +1,6 @@
 import { isArray } from 'typeable'
 
 export default function arrayIn (needle, haystack) {
-  if (!isArray(needle)) return false
+  if (!isArray(haystack)) return false
   return haystack.includes(needle)
 }
