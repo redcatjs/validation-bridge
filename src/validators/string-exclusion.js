002c35cfdfae28e5894b05074ae9aa1077dac8a9
takion <jo@surikat.pro>
2018-09-24 14:32:12 +0200

    fix

--- src/validators/string-exclusion.js
+++ src/validators/string-exclusion.js
@@ -1,4 +1,4 @@
-import { stringInclusion } from './string-inclusion'
+import stringInclusion from './string-inclusion'
 
 export default function stringExclusion (value, options = {}) {
   return !stringInclusion(value, options)
