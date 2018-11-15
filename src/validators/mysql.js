cdb428fb16035162f9fde4c9c925a8d5cbf33bea
takion <jo@surikat.pro>
2018-11-15 07:24:09 +0100

    coerce number to string for mysql validator

--- src/validators/mysql.js
+++ src/validators/mysql.js
@@ -2,6 +2,9 @@ import validator from 'mysql-validator'
 import isScalar from './is-scalar'
 export default function mysql (value, columnType) {
   if (!isScalar(value)) return false
+  if(typeof value==='number'){
+    value = value.toString()
+  }
   return !validator.check(value, columnType)
 }
 mysql.message = 'unexpected mysql value for this column'
